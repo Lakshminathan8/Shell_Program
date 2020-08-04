550b7f04d033c74231799723f26a8724c7255795
Lakshminathan P <LAKSHMINATHAN@Lakshminathans-MacBook-Pro.local>
2020-08-04 23:50:21 +0530

    positional arguments

--- lab01.sh
+++ lab01.sh
@@ -1,1 +1,22 @@
-echo "chai nai hey"
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+echo "Finding largest number"
+echo "enter the value of A"
+read a
+b=200
+c=30
+if [ $a -gt $b ]  && [ $a -gt $c ] 
+then
+echo "A is greater $a"
+elif [ $b -gt $c ] 
+then
+echo "B is greater $b"
+else 
+echo "C is greater $c"
+fi
